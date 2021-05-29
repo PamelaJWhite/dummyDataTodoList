af2798a9432e7e781b7ea7a353d77820d0711ae5
pjwhite08 <pamelajeanwhite@gmail.com>
2021-05-29 17:12:43 -0500

    it works

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 //top section is redo after class
 let arrayOfTodos = [
     {
-    "userId": 14,
+    "userId": 10,
     "id": 1,
+    "title": "delectus aut",
+    "completed": false
+},
+{
+    "userId": 7,
+    "id": 2,
     "title": "delectus aut autem",
     "completed": false
 },
 {
-    "userId": 20,
+    "userId": 8,
     "id": 2,
     "title": "delectus aut autem",
     "completed": false
-}]
-// console.log(arrayOfTodos[0].title)
-// console.log(arrayOfTodos[1].id)
+},
+{
+    "userId": 7,
+    "id": 3,
+    "title": "delectus aut autem",
+    "completed": true
+}
+]
+
+// const listByUserID = arrayOfTodos.filter((arrayOfTodos) => arrayOfTodos.userId === 7) 
+//     console.log(listByUserID)
+
+//    let filteredTodos = listByUserID
+
+//     const listByCompleted = filteredTodos.filter((filteredTodos) => filteredTodos.completed === true ) 
+//     console.log(listByCompleted)
+
+// // console.log(arrayOfTodos[0].title)
+// // console.log(arrayOfTodos[1].id)
+// //let usersID = '' I don't know what this does or doesn't do
 
 const fetchTodos = () => {
     fetch('https://jsonplaceholder.typicode.com/todos')
@@ -29,15 +52,124 @@ const populateTodos = () => {
     for (let index = 0; index < arrayOfTodos.length; index++) {
         const toDoList = arrayOfTodos[index];
         const listItem = document.getElementById("todo-list")
-        console.log("get element by id:" + listItem)
+        // console.log("get element by id:" + listItem)
         const createdLI = document.createElement("LI")
         listItem.appendChild(createdLI)
-        console.log("create element:" + createdLI)
+        // console.log("create element:" + createdLI)
         const text = document.createTextNode(toDoList.title)
         createdLI.appendChild(text)
-        console.log(text)
+    //    console.log(text)
+       //console.log(populateTodos)
+// //         // --------------------------------- after thu
+
     }
 }
+
+let filterTodos = () => {
+    
+    const clear = document.getElementById("todo-list").innerHTML = ""
+    console.log("cleared")
+    let input = document.getElementById("usersID")
+    console.log("input html:" + input)
+    console.log ("value of input:" + input.value)
+
+    const filteredByUserID = arrayOfTodos.filter((arrayOfTodos) => arrayOfTodos.userId == input.value)
+   
+    const secondArray = filteredByUserID
+    console.log("this should be the 2nd array of todos:" + filteredByUserID)
+    for (let index = 0; index < filteredByUserID.length; index++) {
+        const toDoList = filteredByUserID[index];
+        const listItem = document.getElementById("todo-list")
+        // console.log("get element by id:" + listItem)
+        const createdLI = document.createElement("LI") 
+        listItem.appendChild(createdLI)
+        // console.log("create element:" + createdLI)
+        const text = document.createTextNode(toDoList.title)
+        createdLI.appendChild(text)
+    //    console.log(text)
+    }
+    
+}
+
+let sortTodos = () => {
+    const clear = document.getElementById("todo-list").innerHTML = ""
+    console.log("cleared")
+    let input = document.getElementById("usersID")
+    console.log("input html:" + input)
+    console.log ("value of input:" + input.value)
+    const filteredByUserID = arrayOfTodos.filter((arrayOfTodos) => arrayOfTodos.userId == input.value)
+
+    const listByCompleted = filteredByUserID.filter((filteredByUserID) => filteredByUserID.completed === true ) 
+        console.log("list by completed:" + listByCompleted)
+        for (let index = 0; index < listByCompleted.length; index++) {
+            const toDoList = listByCompleted[index];
+            const listItem = document.getElementById("todo-list")
+            // console.log("get element by id:" + listItem)
+            const createdLI = document.createElement("LI") 
+            listItem.appendChild(createdLI)
+            // console.log("create element:" + createdLI)
+            const text = document.createTextNode(toDoList.title)
+            createdLI.appendChild(text)
+        }
+    }
+
+
+//-------------------------------------------
+//store the data in a variable:
+// 
+//const filterTodos = () => {
+//     console.log("filterTodos")
+
+// Or...Is this as simple as?:
+//let originalListData = populateTodos()
+
+//console.log(arrayOfTodos)
+
+//------------------------------------------
+// Clear the previous todos from the view
+//     ????.innerHTML = null
+// 
+//---------------------------------------------
+// Populate it with only todos with the userID that matches the number inputted
+
+   // let listByUserID = arrayOfTodos.filter((arrayOfTodos) => arrayOfTodos.title === "delectus aut autem") //it works until the last part. NO, it's only going through the original listofArrays
+
+//------------------------------
+//Stores the currently filtered to dos in a variable
+
+//let filteredTodos = listByUserID ?????
+//---------------------------------------------------
+//need to populate the list with the filtered todos
+//??????????create text node? append child?
+
+//-----------------------------------------------------
+//sort todos button
+///removes todos from view
+
+//????.innerhtml = null
+
+//-------------------------------------------------------
+//shows only todoes that are either completed or not completed
+//filter by completed
+
+//----------------
+//show those completed
+
+
+  
+
+
+
+    
+
+
+
+
+
+
+
+
+
 
 // //Below is work from class:
 // // We'll pre-populate this array with a couple objects just so it's not undefined if your internet connection isn't working properly.
@@ -100,5 +232,4 @@ const populateTodos = () => {
 
 //     createLi(listName)
     
-// }
-// }
+// 
